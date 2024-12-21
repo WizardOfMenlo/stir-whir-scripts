ab4ac6ead31e64ce2aab8aaeeedbc9912592b9a5
Giacomo Fenzi <giacomofenzi@giacomos-macbook-pro.home>
2024-12-21 14:03:01 +0100

    Pass on STIR

--- src/stir.rs
+++ src/stir.rs
@@ -6,23 +6,37 @@ use crate::{
     LowDegreeParameters,
 };
 
-#[derive(Clone)]
+/// Parameters parametrizing an instance of STIR.
+/// This does not include the entire configuration of STIR, as we populate that later on according to required security config.#[derive(Clone)]
 pub struct StirParameters {
-    // Relate to the first round
+    /// The starting rate used in the protocol.
     pub starting_log_inv_rate: usize,
+
+    /// The folding factor in the first round.
+    /// Given in log form, i.e. starting_folding_factor = 2 implies that the degree is reduced by a factor of 4.
     pub starting_folding_factor: usize,
 
-    // The following relate only to the internal rounds
+    /// The folding factors in the remaining rounds.
+    /// Given in log form, i.e. folding_factors[i] = 2 implies that the degree in round i is reduced by a factor of 4.
     pub folding_factors: Vec<usize>,
+
+    /// The rates used in the internal rounds of STIR.
     pub log_inv_rates: Vec<usize>,
 
+    /// The security assumption under which to configure FRI.
     pub security_assumption: SecurityAssumption,
+
+    /// The security level desired.
     pub security_level: usize,
+
+    /// The number of pow bits to use to reduce query error.
+    /// Traditionally called also "grinding".
+    /// NOTE: This does not affect the pow bits used to reduce proximity gaps errors.
     pub pow_bits: usize,
 }
 
 impl StirParameters {
-    // This is basically a worse FRI (no domain shifting but we use OOD samples and quotients)
+    /// Instantiates a STIR configuration in which the rate is constant. This is a worse version of FRI.
     pub fn fixed_rate_folding(
         log_inv_rate: usize,
         folding_factor: usize,
@@ -42,7 +56,8 @@ impl StirParameters {
         }
     }
 
-    // This is basically the version in the paper
+    /// A STIR configuration in which the domain shrinks by (1/2) in each iteration while the degree shrinks by (1/2^folding_factor).
+    /// This is the version presented in the STIR paper.
     pub fn fixed_domain_shift(
         log_inv_rate: usize,
         folding_factor: usize,
@@ -65,41 +80,71 @@ impl StirParameters {
     }
 }
 
+/// A fully expanded FRI configuration.
 #[derive(Clone)]
 pub struct StirConfig {
+    /// The configuration for the LDT desired.
     pub(crate) ldt_parameters: LowDegreeParameters,
+
+    /// The security assumption under which STIR was configured.
     pub(crate) security_assumption: SecurityAssumption,
+
+    /// The desired security level.
     pub(crate) security_level: usize,
+
+    /// The maximum number of pow bits allowed (over and we throw a warning, as probably then we are misconfigured.)
     pub(crate) max_pow_bits: usize,
 
-    pub(crate) starting_folding_factor: usize,
-    pub(crate) starting_domain_log_size: usize,
+    /// The rate of the inital RS code used during the protocol.    
     pub(crate) starting_log_inv_rate: usize,
+
+    /// The pow bits used in the batching phase.
+    pub batching_pow_bits: f64,
+
+    /// The initial folding factor.
+    pub(crate) starting_folding_factor: usize,
+
+    /// The initial domain size
+    pub(crate) starting_domain_log_size: usize,
+
+    /// The initial pow bits used in the first fold.
     pub(crate) starting_folding_pow_bits: f64,
 
+    /// The round-specific parameters.
     pub(crate) round_parameters: Vec<RoundConfig>,
 
-    pub(crate) final_log_degree: usize,
+    /// Degree of the final polynomial sent over.
+    pub(crate) final_poly_log_degree: usize,
+
+    /// Number of queries in the last round
     pub(crate) final_queries: usize,
+
+    /// Number of final bits of proof of work (for the queries).
     pub(crate) final_pow_bits: f64,
+
+    /// Rate of the final RS codeword.
     pub(crate) final_log_inv_rate: usize,
 }
 
+/// Round specific configuration
 #[derive(Debug, Clone)]
 pub(crate) struct RoundConfig {
+    /// Folding factor for this round.
     pub(crate) folding_factor: usize,
+    /// Size of evaluation domain (of oracle sent in this round)
     pub(crate) evaluation_domain_log_size: usize,
+    /// Number of bits of proof of work (for the queries).
     pub(crate) pow_bits: f64,
+    /// Number of queries in this round
     pub(crate) num_queries: usize,
+    /// Number of OOD samples in this round
     pub(crate) ood_samples: usize,
+    /// Rate of current RS codeword
     pub(crate) log_inv_rate: usize,
 }
 
-fn pow_util(security_level: usize, error: f64) -> f64 {
-    0f64.max(security_level as f64 - error)
-}
-
 impl StirConfig {
+    /// Given a LDT parameter and some parameters for STIR, populate the config.
     pub fn new(ldt_parameters: LowDegreeParameters, stir_parameters: StirParameters) -> Self {
         // We need to fold at least some time
         assert!(
@@ -133,6 +178,21 @@ impl StirConfig {
         let starting_folding_factor = stir_parameters.starting_folding_factor;
         let starting_domain_log_size =
             ldt_parameters.log_degree + stir_parameters.starting_log_inv_rate;
+
+        let batching_pow_bits = if ldt_parameters.batch_size > 1 {
+            // We now start, the initial folding pow bits
+            pow_util(
+                security_level,
+                stir_parameters.security_assumption.prox_gaps_error(
+                    ldt_parameters.log_degree,
+                    stir_parameters.starting_log_inv_rate,
+                    ldt_parameters.field.extension_bit_size(),
+                    ldt_parameters.batch_size.ilog2() as usize, // TODO: This is not correct
+                ),
+            )
+        } else {
+            0.
+        };
 
         // Degree of next polynomial to send
         let mut current_log_degree = ldt_parameters.log_degree - starting_folding_factor;
@@ -231,6 +291,7 @@ impl StirConfig {
             security_assumption: stir_parameters.security_assumption,
             security_level,
             max_pow_bits: stir_parameters.pow_bits,
+            batching_pow_bits,
             starting_folding_factor,
             starting_domain_log_size,
             starting_log_inv_rate: stir_parameters.starting_log_inv_rate,
@@ -238,11 +299,13 @@ impl StirConfig {
             round_parameters,
             final_queries,
             final_pow_bits,
-            final_log_degree,
+            final_poly_log_degree: final_log_degree,
             final_log_inv_rate: log_inv_rate,
         }
     }
 
+    /// Given a configuration, simulate an execution, keeping track of which elements are sent.
+    /// This is used to compute the proof size.
     pub fn build_proof(&self) -> Proof {
         let mut current_merkle_tree = MerkleTree::new(
             self.starting_domain_log_size - self.starting_folding_factor,
@@ -294,7 +357,7 @@ impl StirConfig {
             proof_elements: vec![
                 ProofElement::FieldElements(FieldElements {
                     field: self.ldt_parameters.field,
-                    num_elements: 1 << self.final_log_degree,
+                    num_elements: 1 << self.final_poly_log_degree,
                     is_extension: true,
                 }),
                 ProofElement::MerkleQueries(MerkleQueries {
@@ -307,7 +370,8 @@ impl StirConfig {
         Proof(proof)
     }
 
-    fn print_config_summary(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    /// Prints a summary of the configuration for STIR.
+    pub fn print_config_summary(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "{}", self.ldt_parameters)?;
         writeln!(
             f,
@@ -321,9 +385,17 @@ impl StirConfig {
             self.starting_domain_log_size, self.starting_log_inv_rate,
         )?;
 
+        if self.ldt_parameters.batch_size > 1 {
+            writeln!(
+                f,
+                "Batch size: {}, batching_pow_bits: {:.1}",
+                self.ldt_parameters.batch_size, self.batching_pow_bits
+            )?;
+        }
+
         writeln!(
             f,
-            "Initial folding factor: {}, initial_folding_pow_bits: {}",
+            "Initial folding factor: {}, initial_folding_pow_bits: {:.1}",
             self.starting_folding_factor, self.starting_folding_pow_bits
         )?;
         for r in &self.round_parameters {
@@ -332,17 +404,39 @@ impl StirConfig {
 
         writeln!(
             f,
-            "final_queries: {}, final polynomial: {}, final_rate: 2^-{}, final_pow_bits: {}",
-            self.final_queries, self.final_log_degree, self.final_log_inv_rate, self.final_pow_bits,
+            "final_queries: {}, final polynomial: {}, final_rate: 2^-{}, final_pow_bits: {:.1}",
+            self.final_queries,
+            self.final_poly_log_degree,
+            self.final_log_inv_rate,
+            self.final_pow_bits,
         )?;
 
         Ok(())
     }
 
+    /// Displays the round-by-error analysis for the protocol.
     fn print_rbr_summary(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "------------------------------------")?;
         writeln!(f, "Round by round soundness analysis:")?;
         writeln!(f, "------------------------------------")?;
+
+        // The batching error.
+        if self.ldt_parameters.batch_size > 1 {
+            let batching_prox_gaps_error = self.security_assumption.prox_gaps_error(
+                self.ldt_parameters.log_degree,
+                self.starting_log_inv_rate,
+                self.ldt_parameters.field.extension_bit_size(),
+                self.ldt_parameters.batch_size.ilog2() as usize, // TODO: This is not really right
+            );
+
+            writeln!(
+                f,
+                "{:.1} bits -- batch prox gaps: {:.1}, pow: {:.1}",
+                batching_prox_gaps_error + self.batching_pow_bits,
+                batching_prox_gaps_error,
+                self.batching_pow_bits,
+            )?;
+        }
 
         let mut current_log_degree = self.ldt_parameters.log_degree - self.starting_folding_factor;
         let mut log_inv_rate = self.starting_log_inv_rate;
@@ -439,8 +533,13 @@ impl Display for RoundConfig {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(
             f,
-            "Folding factor: {}, domain_size: 2^{}, num_queries: {}, rate: 2^-{}, pow_bits: {}, ood_samples: {}",
+            "Folding factor: {}, domain_size: 2^{}, num_queries: {}, rate: 2^-{}, pow_bits: {:.1}, ood_samples: {}",
             self.folding_factor, self.evaluation_domain_log_size, self.num_queries, self.log_inv_rate, self.pow_bits, self.ood_samples,
         )
     }
+}
+
+// TODO: Maybe dedup
+fn pow_util(security_level: usize, error: f64) -> f64 {
+    0f64.max(security_level as f64 - error)
 }
