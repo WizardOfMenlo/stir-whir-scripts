d196391403f944a4cfba0d812a680d933d45ee62
Giacomo Fenzi <giacomofenzi@outlook.com>
2024-12-21 14:57:19 +0100

    Fix

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,17 +8,17 @@ use stir_whir_estimation::{
 
 fn main() {
     let ldt_parameters = LowDegreeParameters {
-        field: GOLDILOCKS_3,
-        log_degree: 30,
+        field: GOLDILOCKS_2,
+        log_degree: 26,
         batch_size: 1,
     };
 
     let stir_parameters =
-        StirParameters::fixed_domain_shift(1, 4, 4, SecurityAssumption::JohnsonBound, 128, 20);
+        StirParameters::fixed_domain_shift(1, 4, 4, SecurityAssumption::CapacityBound, 120, 20);
     let stir_config = StirConfig::new(ldt_parameters, stir_parameters);
 
     let fri_parameters =
-        FriParameters::fixed_folding(1, 4, 4, SecurityAssumption::JohnsonBound, 128, 20);
+        FriParameters::fixed_folding(1, 4, 4, SecurityAssumption::CapacityBound, 120, 20);
     let fri_config = FriConfig::new(ldt_parameters, fri_parameters);
 
     println!("{}", stir_config);
